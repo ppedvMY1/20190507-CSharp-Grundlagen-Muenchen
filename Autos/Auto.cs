5bdfdeb7ca0221afcbbfcedf6aeabda8ab3e7214
Markus Yaseen <MarkusY@ppedv.de>
2019-05-08 15:02:06 +0200

    Klasse Autos mit Eigenschaften und Feldern, Konstruktor und überladener ToString

---
+++ Autos/Auto.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autos
+{
+    class Auto
+    {
+        // Eigenschaften und Felder
+        private string farbe;
+        public string Farbe {
+            get
+            {
+                return farbe;
+            }
+            set
+            {
+                // Der eingegebene String muss im Autofarben enum vorhanden sein, ansonsten werfe eine Exception
+                if(Enum.TryParse<Autofarben>(value, out Autofarben auto))
+                {
+                    farbe = value;
+                } else
+                {
+                    throw new Exception();
+                }
+            }
+        }
+        public enum Autofarben
+        {
+            Rot,
+            Blau,
+            Schwarz,
+            Weiß,
+            Silber,
+            Grün
+        }
+        private string kennzeichen;
+        public string Kennzeichen
+        {
+            get
+            {
+                return kennzeichen;
+            }
+            set
+            {
+                if (value.Length > 2)
+                {
+                    kennzeichen = value;
+                }else
+                {
+                    throw new Exception();
+                }
+            }
+        }
+        private int ps;
+        public int Ps
+        {
+            get
+            {
+                return ps;
+            }
+            set
+            {
+                if (value > 0 && value <= 1500)
+                {
+                    ps = value;
+                }else
+                {
+                    throw new Exception();
+                }
+            }
+        }
+        private int baujahr;
+        public int Baujahr
+        {
+            get
+            {
+                return baujahr;
+            }
+            set
+            {
+                if (value >= 1885 && value <= System.DateTime.Now.Year)
+                {
+                    baujahr = value;
+                } else
+                {
+                    throw new Exception();
+                }
+            }
+        }
+
+        public Auto(string farbe, string kennzeichen, int ps, int baujahr)
+        {
+            Farbe = farbe;
+            Kennzeichen = kennzeichen;
+            Ps = ps;
+            Baujahr = baujahr;
+        }
+
+        //ToString von Object überschreiben, damit wir eine bessere Ausgabe erhalten
+        public override string ToString()
+        {
+            return $"Das Auto hat die Farbe {this.Farbe} und das Kennzeichen {this.Kennzeichen}, hat {this.Ps} PS und wurde im Jahr {this.Baujahr} gebaut";
+        }
+
+    }
+}
---
+++ Autos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Auto Objekte erstellen
+            Auto auto1 = new Auto("Rot", "M AV 123874", 20, 1899);
+            Auto auto2 = new Auto("Blau", "M TL 342", 20, 1899);
+            Auto auto3 = new Auto("Grün", "M XY 1294", 20, 1899);
+
+            // Ruft die überschriebene ToString() Methode auf und gibt sie in der Konsole aus
+            Console.WriteLine($"Auto1: {auto1}");
+            Console.WriteLine($"Auto2: {auto2}");
+            Console.WriteLine($"Auto3: {auto3}");
+
+            auto1.Farbe = "Silber";
+            Console.WriteLine($"Auto1: {auto1}");
+
+            Console.ReadKey();
+            System.Threading.Thread.Sleep(1000);
+        }
+    }
+}
