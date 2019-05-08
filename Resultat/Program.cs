46247ee83d7f60e32ea259279a47e1d90ed59376
Markus Yaseen <MarkusY@ppedv.de>
2019-05-08 15:01:03 +0200

    TestResultet mit Do While

--- Resultat/Program.cs
+++ Resultat/Program.cs
@@ -11,23 +11,27 @@ namespace TestResultat
         static void Main(string[] args)
         {
             int eingegebeneZahl;
+            do
+            {
+                // Die Schleife läuft so lange weiter, bis der Nutzer eine gültige Zahl eingegeben hat
+                do
+                {
+                    Console.WriteLine("Bitte gib eine Zahl ein: ");
+                } while (!int.TryParse(Console.ReadLine(), out eingegebeneZahl));
 
-            // Die Schleife läuft so lange weiter, bis der Nutzer eine gültige Zahl eingegeben hat
-            while(!int.TryParse(Console.ReadLine(), out eingegebeneZahl))
-            {
-                Console.WriteLine("Bitte gib eine Zahl ein: ");
-            }
-
-            if(eingegebeneZahl >= 7 && eingegebeneZahl <= 10)
-            {
-                Console.WriteLine("Der Test ist bestanden!");
-            } else if (eingegebeneZahl < 7 && eingegebeneZahl >= 1)
-            {
-                Console.WriteLine("Der Test ist leider nicht bestanden!");
-            } else
-            {
-                Console.WriteLine("Ungültige Eingabe");
-            }
+                if (eingegebeneZahl >= 7 && eingegebeneZahl <= 10)
+                {
+                    Console.WriteLine("Der Test ist bestanden!");
+                }
+                else if (eingegebeneZahl < 7 && eingegebeneZahl >= 1)
+                {
+                    Console.WriteLine("Der Test ist leider nicht bestanden!");
+                }
+                else
+                {
+                    Console.WriteLine("Ungültige Eingabe");
+                }
+            } while (eingegebeneZahl != -1);
 
             Console.ReadKey();
         }
