0225942c67007d30bfb63444a31722ea989a0c91
Markus Yaseen <MarkusY@ppedv.de>
2019-05-09 12:00:57 +0200

    Taschenrechner Grundlagen

---
+++ Taschenrechner/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Taschenrechner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welche Rechenoperation möchtest du durchführen?");
+            var operation = Console.ReadLine();
+
+            Console.WriteLine("Bitte gib den ersten Operanden ein:");
+            var op1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Bitte gib den zweiten Operanden ein:");
+            var op2 = int.Parse(Console.ReadLine());
+
+            Rechner rechner = new Rechner("");
+            Console.WriteLine("Das Ergebnis lautet:");
+            Console.WriteLine(rechner.Rechne(op1, op2));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taschenrechner/Rechner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Taschenrechner
+{
+    class Rechner
+    {
+        public Rechner(string operation)
+        {
+
+        }
+        public int Rechne(int op1, int op2)
+        {
+            return op1 + op2;
+        }
+    }
+}
