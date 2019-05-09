8cecf2a4c4ea8a92bbca3237a266484fc53c14cf
Markus Yaseen <MarkusY@ppedv.de>
2019-05-09 11:53:38 +0200

    Array, ArrayList, List mit Bsp

---
+++ CSharpAnfänger/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAnfänger
+{
+    class Person
+    {
+        private string nachname;
+        public string Nachname { get; } = "DEF";
+        
+        public Person()
+        {
+            Nachname = "ABC";
+            ABC();
+        }
+        public void ABC()
+        {
+            Console.WriteLine(Nachname);
+        }
+    }
+}
--- CSharpAnfänger/Program.cs
+++ CSharpAnfänger/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,48 @@ namespace CSharpAnfänger
     {
         static void Main(string[] args)
         {
+            //Array
+            string[] stringArray = new string[10];
+            //ArrayList
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add("String 1");
+            arrayList.Add("String 2");
+            arrayList.Add("5");
+            arrayList.Add(2);
+            arrayList.Add(new Random());
+            arrayList.Add(new object());
+            arrayList.Add(true);
+
+            foreach (var item in arrayList)
+            {
+                if(item.GetType() == typeof(string))
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+            ArrayList intArrayList = new ArrayList();
+            intArrayList.Add(1);
+            intArrayList.Add(2);
+            intArrayList.Add(3);
+
+            //List
+            List<string> stringList = new List<string>();
+            stringList.Add("String 1");
+            stringList.Add("String 2");
+            foreach (var item in stringList)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Stack
+            //Queue
+
+            //HashTable
+            //Dictionary
+
+
+            Console.ReadKey();
         }
     }
 }
