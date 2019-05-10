ed37bfe5a3348185b6f49f7fd846f0f3f25c1ee2
Markus Yaseen <MarkusY@ppedv.de>
2019-05-10 12:18:38 +0200

    Texteditor mit Datei lesen, speichern und filedialog und XML Format

---
+++ Wpf/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Wpf
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Text Text;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Text = new Text();
+            DataContext = Text;
+        }
+
+        private void Speichern_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.FileName = "inhalt.bin";
+            saveFileDialog.Filter = "Textdatei|*.bin";
+            saveFileDialog.InitialDirectory = Directory.GetCurrentDirectory();
+            saveFileDialog.RestoreDirectory = true;
+            if ((bool)saveFileDialog.ShowDialog())
+            {
+                using (BinaryWriter binaryWriter = new BinaryWriter(File.Open(saveFileDialog.FileName, FileMode.OpenOrCreate)))
+                {
+                    XmlSerializer xmlSerializer = new XmlSerializer(typeof(Text));
+                    xmlSerializer.Serialize(binaryWriter.BaseStream, Text);
+                }
+            }
+        }
+
+        private void Oeffnen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.FileName = "inhalt.bin";
+            openFileDialog.Filter = "Textdatei|*.bin";
+            openFileDialog.InitialDirectory = Directory.GetCurrentDirectory();
+            openFileDialog.RestoreDirectory = true;
+            if ((bool)openFileDialog.ShowDialog())
+            {
+                using (BinaryReader binaryReader = new BinaryReader(File.Open(openFileDialog.FileName, FileMode.OpenOrCreate)))
+                {
+                    XmlSerializer xmlSerializer = new XmlSerializer(typeof(Text));
+                    Text = (Text)xmlSerializer.Deserialize(binaryReader.BaseStream);
+                    DataContext = Text;
+                }
+            }
+        }
+    }
+}
---
+++ Wpf/Text.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf
+{
+    public class Text
+    {
+        public string TextInhalt { get; set; }
+        public string TextFarbe { get; set; } = "Black";
+    }
+}
